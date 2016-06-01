822ccb199881a8f933e707ad6ebd677ce21a1315
arth-complitech <arth@complitech.net>
2016-06-01 13:02:48 +0530

    Spec added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
-describe User do
+require 'user'
+RSpec.describe User, :type => :model do
 	it "is invalid without email" do
-	 	user=build(:user,email: nil)
+	 	user=build(email: nil)
 		expect(user).to have(1).errors_on(:email)
 	end
 
 	it "is invalid without first_name" do
-	 	user=build(:user,first_name: nil)
+	 	user=build(first_name: nil)
 		expect(user).to have(1).errors_on(:first_name)
 	end
 
 	it "is invalid without last_name" do
-	 	user=build(:user,last_name: nil)
+	 	user=build(last_name: nil)
 		expect(user).to have(1).errors_on(:last_name)
 	end
 end
