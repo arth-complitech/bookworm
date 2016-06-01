ad3f5bda0f8539c1fd4e2c7f78b29bc693d94322
arth-complitech <arth@complitech.net>
2016-06-01 12:10:00 +0530

    Spec files for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe User do
+	it "is invalid without email" do
+	 	user=build(:user,email: nil)
+		expect(user).to have(1).errors_on(:email)
+	end
+
+	it "is invalid without first_name" do
+	 	user=build(:user,first_name: nil)
+		expect(user).to have(1).errors_on(:first_name)
+	end
+
+	it "is invalid without last_name" do
+	 	user=build(:user,last_name: nil)
+		expect(user).to have(1).errors_on(:last_name)
+	end
+end
